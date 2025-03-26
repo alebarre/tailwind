031b4d7d6831bb82667c244cfde7205cf269291d
Alexandre Barreto <plotartemail@gmail.com>
2025-03-26 15:56:10 -0300

    Commit fnal com correção de css hambuerger menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.html'],
   theme: {
     screens: {
       sm: '480px',
       md: '768px',
-      lg: '976',
-      xl: '1440'
+      lg: '976px',
+      xl: '1440px',
     },
     extend: {
       fontFamily: {
         sans: ['Josefin Sans', 'sans-serif'],
-        alata: ['Alata']
+        alata: ['Alata'],
       },
       letterSpacing: {
-        widest: '.3em'
+        widest: '.3em',
       },
     },
   },
   plugins: [],
 }
-
